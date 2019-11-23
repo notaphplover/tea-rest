7139b1ad9e5faee9573751a7dca3cde7afe37251
Roberto Pintos López <roberto@secture.com>
2019-11-23 08:31:00 +0100

    Added guardian kid relation.

--- src/Component/Person/Service/KidManager.php
+++ src/Component/Person/Service/KidManager.php
@@ -2,29 +2,45 @@
 
 namespace App\Component\Person\Service;
 
+use App\Entity\GuardianKidRelation;
 use App\Entity\Kid;
+use App\Repository\GuardianKidRelationRepository;
 use App\Repository\KidRepository;
 
 class KidManager
 {
     /**
+     * @var GuardianKidRelationRepository
+     */
+    protected $guardianKidRelationRepository;
+    /**
      * @var KidRepository
      */
     protected $kidRepository;
 
-    public function __construct(KidRepository $kidRepository)
-    {
+    public function __construct(
+        GuardianKidRelationRepository $guardianKidRelationRepository,
+        KidRepository $kidRepository
+    ) {
+        $this->guardianKidRelationRepository = $guardianKidRelationRepository;
         $this->kidRepository = $kidRepository;
     }
 
     /**
-     * @param Kid $kid
+     * @param Kid $entity
      * @param bool $commit
      * @throws \Doctrine\ORM\ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function update(Kid $kid, bool $commit = true): void
+    public function update(Kid $entity, bool $commit = true): void
     {
-        $this->kidRepository->update($kid, $commit);
+        if (!$this->kidRepository->isManaged($entity)) {
+            $guardianKidRelation = (new GuardianKidRelation())
+                ->setGuardian($entity->getGuardian())
+                ->setKid($entity)
+            ;
+            $this->guardianKidRelationRepository->update($guardianKidRelation, false);
+        }
+        $this->kidRepository->update($entity, $commit);
     }
 }
---
+++ src/Repository/GuardianKidRelationRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\GuardianKidRelation;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * Class GuardianKidRelationRepository
+ * @package App\Repository
+ *
+ * @method findOneBy(array $criteria, array $orderBy = null): ?GuardianKidRelation
+ * @method isManaged(GuardianKidRelation $entity) : bool
+ * @method update(GuardianKidRelation $entity, bool $commit = true) : void
+ */
+class GuardianKidRelationRepository extends BaseRepository
+{
+    /**
+     * GuardianKidRelationRepository constructor.
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, GuardianKidRelation::class);
+    }
+}
