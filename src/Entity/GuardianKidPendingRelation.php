e442a769629c56665b1566e3974fa52f61c6aaa1
Roberto Pintos López <roberto@secture.com>
2019-11-24 09:51:19 +0100

    Added unique constraints.

--- src/Entity/GuardianKidPendingRelation.php
+++ src/Entity/GuardianKidPendingRelation.php
@@ -7,7 +7,13 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=GuardianKidPendingRelationRepository::class)
- * @ORM\Table(name="guardian_kid_pending_relation")
+ * @ORM\Table(
+ *     name="guardian_kid_pending_relation",
+ *     uniqueConstraints={
+ *        @ORM\UniqueConstraint(name="guardian_kid_unique",
+ *            columns={"guardian_id", "kid_id"})
+ *    }
+ * )
  *
  * Class GuardianKidPendingRelation
  * @package App\Entity
--- src/Entity/GuardianKidRelation.php
+++ src/Entity/GuardianKidRelation.php
@@ -7,7 +7,13 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=GuardianKidRelationRepository::class)
- * @ORM\Table(name="guardian_kid_relation")
+ * @ORM\Table(
+ *     name="guardian_kid_relation",
+ *     uniqueConstraints={
+ *        @ORM\UniqueConstraint(name="guardian_kid_unique",
+ *            columns={"guardian_id", "kid_id"})
+ *    }
+ * )
  */
 class GuardianKidRelation extends GuardianKidRelationBase
 {
