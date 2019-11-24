6c221d8942b23a212d1347b806bb82a01586d3d9
Roberto Pintos López <roberto@secture.com>
2019-11-24 09:29:55 +0100

    Added validation.
    Added a class to validate a kid association request.

---
+++ src/Component/Person/Validation/KisAssociationRequestValidation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Component\Person\Validation;
+
+use App\Component\Common\Validation\BaseValidation;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class KisAssociationRequestValidation extends BaseValidation
+{
+    public const FIELD_NICKNAME = 'nick';
+
+    /**
+     * LoginValidation constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            new Assert\Collection([
+                self::FIELD_NICKNAME => [
+                    new Assert\Type(['type' => ['string']]),
+                    new Assert\Required(),
+                ],
+            ])
+        );
+    }
+}
