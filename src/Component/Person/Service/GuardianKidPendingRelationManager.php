3941fb70a4037163c81257d8b24014026299fcc6
Roberto Pintos López <roberto@secture.com>
2019-11-26 09:03:03 +0100

    Added manager operations.

--- src/Component/Person/Service/GuardianKidPendingRelationManager.php
+++ src/Component/Person/Service/GuardianKidPendingRelationManager.php
@@ -2,9 +2,14 @@
 
 namespace App\Component\Person\Service;
 
+use App\Component\Common\Exception\ResourceNotFoundException;
+use App\Component\Person\Exception\KidAssociationAlreadyExists;
 use App\Entity\GuardianKidPendingRelation;
+use App\Entity\GuardianKidRelation;
 use App\Repository\GuardianKidPendingRelationRepository;
+use App\Repository\GuardianKidRelationRepository;
 use App\Repository\GuardianRepository;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 /**
  * Class GuardianKidPendingRelationManager
@@ -17,14 +22,62 @@ use App\Repository\GuardianRepository;
 class GuardianKidPendingRelationManager extends GuardianKidRelationBaseManager
 {
     /**
+     * @var GuardianKidRelationRepository
+     */
+    protected $guardianKidRelationRepository;
+
+    /**
      * GuardianKidPendingRelationManager constructor.
      * @param GuardianKidPendingRelationRepository $guardianKidRelationBaseRepository
+     * @param GuardianKidRelationRepository $guardianKidRelationRepository
      * @param GuardianRepository $guardianRepository
      */
     public function __construct(
         GuardianKidPendingRelationRepository $guardianKidRelationBaseRepository,
+        GuardianKidRelationRepository $guardianKidRelationRepository,
         GuardianRepository $guardianRepository
     ) {
         parent::__construct($guardianKidRelationBaseRepository, $guardianRepository);
+
+        $this->guardianKidRelationRepository = $guardianKidRelationRepository;
+    }
+
+    /**
+     * @param int $id
+     * @throws KidAssociationAlreadyExists
+     * @throws ResourceNotFoundException
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function accept(int $id): void
+    {
+        $relation = $this->getEntityRepository()->getById($id);
+        if (null === $relation) {
+            throw new ResourceNotFoundException();
+        }
+        $newRelation = (new GuardianKidRelation())
+            ->setGuardian($relation->getGuardian())
+            ->setKid($relation->getKid());
+
+        try {
+            $this->guardianKidRelationRepository->update($newRelation);
+        } catch (UniqueConstraintViolationException $exception) {
+            throw new KidAssociationAlreadyExists($newRelation->getGuardian(), $newRelation->getKid(), $exception);
+        }
+    }
+
+    /**
+     * @param int $id
+     * @throws ResourceNotFoundException
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function reject(int $id): void
+    {
+        $relation = $this->getEntityRepository()->getById($id);
+        if (null === $relation) {
+            throw new ResourceNotFoundException();
+        }
+        $this->getEntityRepository()->remove($relation, true);
     }
 }
