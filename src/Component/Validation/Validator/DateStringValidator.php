170b13cecca1571c59d111b6c507d558a6f78207
Roberto Pintos López <roberto@secture.com>
2019-11-27 15:49:27 +0100

    Improved validators.

--- src/Component/Validation/Validator/DateStringValidator.php
+++ src/Component/Validation/Validator/DateStringValidator.php
@@ -2,13 +2,14 @@
 
 namespace App\Component\Validation\Validator;
 
-use App\Component\Validation\Constraint\DateTimeStringConstraint;
+use App\Component\Validation\Constraint\DateStringConstraint;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
-class DateTimeStringValidator extends ConstraintValidator
+class DateStringValidator extends ConstraintValidator
 {
+    private const SECONDS_PER_DAY = 24 * 60 * 60;
     /**
      * @param mixed $value
      * @param Constraint $constraint
@@ -16,11 +17,14 @@ class DateTimeStringValidator extends ConstraintValidator
      */
     public function validate($value, Constraint $constraint): void
     {
-        if (!$constraint instanceof DateTimeStringConstraint) {
-            throw new UnexpectedTypeException($constraint, DateTimeStringConstraint::class);
+        if (!$constraint instanceof DateStringConstraint) {
+            throw new UnexpectedTypeException($constraint, DateStringConstraint::class);
         }
         try {
-            new \DateTime($value);
+            $dateTime = new \DateTime($value);
+            if ($dateTime->getTimestamp() % self::SECONDS_PER_DAY !== 0) {
+                $this->context->buildViolation($constraint->message)->addViolation();
+            }
         } catch (\Exception $exception) {
             $this->context->buildViolation($constraint->message)->addViolation();
         }
