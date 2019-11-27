9370f1de79a15f24e32025416c92a96b8022bd0d
Roberto Pintos López <roberto@secture.com>
2019-11-27 15:51:00 +0100

    ConcreteSubTaskManager now uses the right repository.

--- src/Component/Calendar/Service/ConcreteSubTaskManager.php
+++ src/Component/Calendar/Service/ConcreteSubTaskManager.php
@@ -2,7 +2,12 @@
 
 namespace App\Component\Calendar\Service;
 
+use App\Repository\ConcreteSubTaskRepository;
+
 class ConcreteSubTaskManager extends TaskFragmentBaseManager
 {
-
+    public function __construct(ConcreteSubTaskRepository $entityRepository)
+    {
+        parent::__construct($entityRepository);
+    }
 }
