1905da1d4af551d909cbfb3304a00cfbd6e1d702
Roberto Pintos López <roberto@secture.com>
2019-11-28 10:41:29 +0100

    Added repo query to detect collisions.

--- src/Component/Calendar/Service/TaskBaseManager.php
+++ src/Component/Calendar/Service/TaskBaseManager.php
@@ -2,6 +2,22 @@
 
 namespace App\Component\Calendar\Service;
 
+use App\Entity\TaskBase;
+use App\Repository\TaskBaseRepository;
+
+/**
+ * @method TaskBaseRepository getEntityRepository()
+ */
 abstract class TaskBaseManager extends TaskFragmentBaseManager
 {
+    /**
+     * @param TaskBase $task
+     * @return bool
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function areCollisions(TaskBase $task): bool
+    {
+        return $this->getEntityRepository()->areCollisions($task);
+    }
 }
--- src/Repository/TaskBaseRepository.php
+++ src/Repository/TaskBaseRepository.php
@@ -2,6 +2,32 @@
 
 namespace App\Repository;
 
+use App\Entity\TaskBase;
+
 abstract class TaskBaseRepository extends TaskFragmentBaseRepository
 {
+    /**
+     * @param TaskBase $task
+     * @return bool
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function areCollisions(TaskBase $task): bool
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $query = $qb
+            ->select('count(t)')
+            ->where(
+                $qb->expr()->andX(
+                    $qb->expr()->lt('t.timeStart', ':timeEnd'),
+                    $qb->expr()->gt('t.timeEnd', ':timeStart')
+                )
+            )->setParameters([
+                'timeStart' => $task->getTimeStart(),
+                'timeEnd' => $task->getTimeEnd()
+            ])->getQuery();
+
+        return $query->getSingleScalarResult() > 0;
+    }
 }
