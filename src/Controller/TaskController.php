e6eeb267272e32a68fd5f7316e031ae88a7700b3
Roberto Pintos López <roberto@secture.com>
2019-11-28 13:08:39 +0100

    Added docs.

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,8 +5,11 @@ namespace App\Controller;
 use App\Component\Auth\Entity\TokenUser;
 use App\Component\Calendar\Handler\CreateTasksHandler;
 use App\Component\Serialization\Service\SerializationProvider;
+use App\Entity\ConcreteTask;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +22,85 @@ class TaskController extends AbstractFOSRestController
     use ControllerHelper;
 
     /**
+     * @SWG\Post(
+     *     tags={"calendar"},
+     *     security={{"ApiToken": {}}},
+     *     consumes={"application/json"},
+     *     description="It creates new task for a kid at a certain day.",
+     *     @SWG\Parameter(
+     *          name="createTasjsData",
+     *          in="body",
+     *          required=true,
+     *          description="JSON object",
+     *          @SWG\Schema(
+     *              type="object",
+     *              required={"day", "kid", "tasks"},
+     *              @SWG\Property(
+     *                  property="day",
+     *                  type="string",
+     *                  example="2010-11-22",
+     *                  description="Taks date"
+     *              ),
+     *              @SWG\Property(
+     *                  property="kid",
+     *                  type="int",
+     *                  example="1",
+     *                  description="Kid's identifier"
+     *              ),
+     *              @SWG\Property(
+     *                  property="tasks",
+     *                  type="array",
+     *                  description="Kid's tasks to create",
+     *                  @SWG\Items(
+     *                      type="object",
+     *                      required={"end", "steps", "start"},
+     *                      @SWG\Property(
+     *                          property="end",
+     *                          type="string",
+     *                          example="10:55",
+     *                          description="Task end time"
+     *                      ),
+     *                      @SWG\Property(
+     *                          property="start",
+     *                          type="string",
+     *                          example="10:25",
+     *                          description="Task start time"
+     *                      ),
+     *                      @SWG\Property(
+     *                          property="steps",
+     *                          type="array",
+     *                          description="Task steps. A task must contain at least one step",
+     *                          @SWG\Items(
+     *                              type="object",
+     *                              required={"image", "text"},
+     *                              @SWG\Property(
+     *                                  property="image",
+     *                                  type="string",
+     *                                  example="https://sample.com/image-sample",
+     *                                  description="Step's image"
+     *                              ),
+     *                              @SWG\Property(
+     *                                  property="text",
+     *                                  type="string",
+     *                                  example="An epic action!",
+     *                                  description="Step's text"
+     *                              )
+     *                          )
+     *                      ),
+     *                  )
+     *              )
+     *          )
+     *     ),
+     *     @SWG\Response(
+     *          response="200",
+     *          description="The tasks were created successfully.",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @Model(type=ConcreteTask::class, groups={"concrete-task-common"})
+     *         )
+     *     )
+     * )
+     *
      * @Rest\Post("")
      *
      * @param CreateTasksHandler $createTasksHandler
